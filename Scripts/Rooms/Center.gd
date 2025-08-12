5e15db284987f122cdf55250c563d4e665af1c9e
lucascarrug <lucascarrizoruggeri2004@gmail.com>
2025-08-12 22:10:50 +0200

    Rooms exploration added

---
+++ Scripts/Rooms/Center.gd
@@ -0,0 +1,16 @@
+extends Control
+
+func _on_down_button_pressed() -> void:
+	SceneManager.load_new_scene("res://Scenes/Down.tscn", get_tree().root, self, "fade_in")
+
+
+func _on_up_button_pressed() -> void:
+	SceneManager.load_new_scene("res://Scenes/Up.tscn", get_tree().root, self, "fade_in")
+
+
+func _on_right_button_pressed() -> void:
+	SceneManager.load_new_scene("res://Scenes/Right.tscn", get_tree().root, self, "fade_in")
+
+
+func _on_left_button_pressed() -> void:
+	SceneManager.load_new_scene("res://Scenes/Left.tscn", get_tree().root, self, "fade_in")
